7789c7682ecbb1025c3bf14b8c95918d40f7c8d5
Alex <mr.taff@ya.ru>
2020-10-20 16:54:59 +0300

    first commit

---
+++ src/components/Table.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="table">
+    <h1>{{ title }}</h1>
+    <div class="table-container">
+        <div class="table-content">
+            <el-row :gutter="20">
+              <el-col :span="20">
+                <el-input placeholder="Please input to Search" v-model="search" clearable></el-input>
+              </el-col>
+              <el-col :span="4">
+                <el-select v-model="value" placeholder="Select by">
+                  <el-option v-for="item in selectOptions" :key="item.value" :label="item.label" :value="item.value" class="table-select"></el-option>
+                </el-select>
+              </el-col>
+            </el-row>
+            <el-table :data="searchedData">
+              <el-table-column prop="id" label="ID" width="180"></el-table-column>
+              <el-table-column prop="name" label="Name" width="180"></el-table-column>
+              <el-table-column prop="username" label="Username"></el-table-column>
+              <el-table-column prop="email" label="E-mail"></el-table-column>
+              <el-table-column prop="phone" label="Phone"></el-table-column>
+            </el-table>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    title: String
+  },
+  methods: {
+    fetchData() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => { this.tableData = json })
+    }
+  },
+  computed: {
+    searchedData() {
+      return this.tableData.filter(elem => {
+        return elem.name.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  },
+  data() {
+    return {
+      tableData: [],
+      search: '',
+      selectOptions: [{
+          value: 'id',
+          label: 'ID'
+        }, {
+          value: 'name',
+          label: 'Name'
+        }, {
+          value: 'username',
+          label: 'Username'
+        }, {
+          value: 'email',
+          label: 'Email'
+        }, {
+          value: 'phone',
+          label: 'Phone'
+        }],
+        selectValue: ''
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
+}
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.table-container {
+  display: flex;
+  justify-content: center;
+}
+
+.table-content {
+  padding: 10px;
+  width: 70%;
+}
+
+.table-select {
+  display: block;
+}
+
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ aboutTite }}</h1>
+    <div class="about-text">
+      <p class="text">{{ aboutText }}</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      aboutTite: 'О таблицах',
+      aboutText: `Таблица состоит из упорядоченного набора строк и столбцов:
+            элементы строки сравниваются между собой и имеют смысловую связь
+            в столбце перечислены свойства этих элементов, по которым они сравниваются
+            обычно столбец имеет название;
+            его название может состоять из слова, фразы или числа;
+            пересечением строки и столбца является ячейка таблицы.
+            Элементы таблицы могут быть по-разному сгруппированы или расположены. Кроме того, таблица может включать метаданные, аннотации, заголовок, колонтитул.`
+    }
+  }
+}
+</script>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+}
+
+.about-text {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.text {
+  font-size: 2rem;
+  width: 50%;
+}
+</style>
